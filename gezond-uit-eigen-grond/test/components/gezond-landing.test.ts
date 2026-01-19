ebc9dc7a50ebefca78b6831174659aee9c934d6f
Kenzo Van Craeynest <keno.vancraeynest@vlaanderen.be>
2026-01-19 11:02:27 +0100

    chore: vervang alle verwijzingen naar 'jubel' door 'gezond uit eigen grond'

--- gezond-uit-eigen-grond/test/components/gezond-landing.test.ts
+++ gezond-uit-eigen-grond/test/components/gezond-landing.test.ts
@@ -1,20 +1,20 @@
 import { getShadowRoot } from '../test-util';
 
-describe('jubel-landing', (): void => {
-  const JUBEL_LANDING_TAG: string = 'jubel-landing';
-  const TITLE_ID: string = 'jubel-landing-title';
+describe('gezond-landing', (): void => {
+  const GEZOND_LANDING_TAG: string = 'gezond-landing';
+  const TITLE_ID: string = 'gezond-landing-title';
 
   beforeEach((): void => {
-    document.body.innerHTML = '<jubel-landing></jubel-landing>';
+    document.body.innerHTML = '<gezond-landing></gezond-landing>';
   });
 
   afterEach((): void => {
-    document.body.getElementsByTagName(JUBEL_LANDING_TAG)[0].remove();
+    document.body.getElementsByTagName(GEZOND_LANDING_TAG)[0].remove();
   });
 
   it('renders title', (): void => {
-    const expectedText: string = 'Jubel';
-    const renderedText: string = getShadowRoot(JUBEL_LANDING_TAG)!.getElementById(TITLE_ID)!.innerText;
+    const expectedText: string = 'Gezond uit eigen grond';
+    const renderedText: string = getShadowRoot(GEZOND_LANDING_TAG)!.getElementById(TITLE_ID)!.innerText;
     expect(expectedText).toEqual(renderedText);
   });
 });
